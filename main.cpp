7715aca89db5768f91b7e6059828df89e02c04d0
OleksandrBazhyn <oleksandr_bazhyn@knu.ua>
2023-11-05 21:15:13 +0200

    init

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
+// Bazhyn Oleksandr K-24 variant 1
+// scheme 1, nt = 5, a = 6, b = 5, c = 8, d = 5, e = 8, f = 9, g = 4, h = 5, i = 6, j = 8
+// scheme 1:  a->c->g->j, a->d->h->j, b->e->h->j, b->f->i->j
+
 #include <thread>
 #include <semaphore>
 #include <latch>
 #include <barrier>
 
+#include <vector>
+#include <string>
+#include <iostream>
+#include <syncstream>
+#include <functional>
+
 const int nt = 5;
 
+
+struct A
+{
+
+};
 
 int main()
 {
