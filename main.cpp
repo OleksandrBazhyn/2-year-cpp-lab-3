eafc5238907a5a8b6c639bdfad477dc8ee5172ae
OleksandrBazhyn <oleksandr_bazhyn@knu.ua>
2023-11-07 21:57:59 +0200

    realized dugas and design of orgraph

--- main.cpp
+++ main.cpp
@@ -17,66 +17,26 @@
 #include "actionsSets.cpp"
 #include "Duga.h"
 
+typedef std::unordered_map<char, std::vector<std::function<std::string()>>> Orgraph;
+
 const int nt = 5;
 
-//static std::string f(char x, int i)
-//{
-//	switch (x)
-//	{
-//		case 'a':
-//			return ;
-//		break;
-//		case 'b':
-//			return ;
-//		break;
-//	default:
-//		break;
-//	}
-//	return;
-//}
+std::vector<std::function<std::string()>> fillDuga(int countOfActs, std::function<std::string()>  func)
+{
+	std::vector<std::function<std::string()>> duga;
 
-//std::string GetResult()
-//{
-//	std::vector<Duga> duga;
-//	duga.push_back(A());
-//	duga.push_back(B());
-//	duga.push_back(C());
-//	duga.push_back(D());
-//	duga.push_back(E());
-//	duga.push_back(F());
-//	duga.push_back(G());
-//	duga.push_back(H());
-//	duga.push_back(I());
-//	duga.push_back(J());
-//
-//	std::vector<std::thread> threads;
-//	std::vector<std::string> results;
-//
-//	std::latch latch(nt);
-//	std::barrier barrier(nt);
-//
-//	for (int i = 0; i < nt; i++)
-//	{
-//		threads.push_back(std::thread([&duga, &results, &latch, &barrier, i]()
-//			{
-//				std::string result = duga[i].GetResult();
-//				results.push_back(result);
-//				latch.count_down();
-//				barrier.arrive_and_wait();
-//			}));
-//	}
-//
-//	latch.wait();
-//
-//	std::string result = "";
-//	for (int i = 0; i < nt; i++)
-//	{
-//		result += results[i];
-//	}
-//
-//	return result;
-//
-//}
+	for (int i = 0; i < countOfActs; i++)
+	{
+		duga.push_back(func);
+	}
+
+	return duga;
+}
+
+std::string f(char x, int i)
+{
+	return "v";
+}
 
 int main()
 {
@@ -84,36 +44,41 @@ int main()
 	std::string start1 = " ";
 	std::string start2 = " ";
 
-	auto A = [&]() {
-		return start1 + "A";
-		};
-	auto B = [&]() {
-		return start1 + "B";
-		};
-	auto C = [&]() {
-		return A() + "C";
-		};
-	auto D = [&]() {
-		return A() + "D";
-		};
-	auto E = [&]() {
-		return B() + "E";
-		};
-	auto F = [&]() {
-		return B() + "F";
-		};
-	auto G = [&]() {
-		return C() + "G";
-		};
-	auto H = [&]() {
-		return D() + E() + "H";
-		};
-	auto I = [&]() {
-		return F() + "I";
-		};
-	auto J = [&]() {
-		return G() + H() + I() + "J";
-		};
+	Orgraph orgraph;
 
-	std::cout << J();
+	orgraph['a'] = fillDuga(6, [&]() {
+		return start1 + "A";
+		});
+
+	orgraph['b'] = fillDuga(5, [&]() {
+		return start1 + "B";
+		});
+	orgraph['c'] = fillDuga(8, [&]() {
+		return f('a', 6) + "C";
+		});
+	orgraph['d'] = fillDuga(5, [&]() {
+		return f('a', 6) + "D";
+		});
+	orgraph['e'] = fillDuga(8, [&]() {
+		return f('b', 5) + "E";
+		});
+	orgraph['f'] = fillDuga(9, [&]() {
+		return f('b', 5) + "F";
+		});
+	orgraph['g'] = fillDuga(4, [&]() {
+		return f('c', 8) + "G";
+		});
+	orgraph['h'] = fillDuga(5, [&]() {
+		return f('d', 5) + f('e', 8) + "H";
+		});
+	orgraph['i'] = fillDuga(6, [&]() {
+		return f('f', 9) + "I";
+		});
+	orgraph['j'] = fillDuga(8, [&]() {
+		return f('g', 4) + f('h', 5) + f('i', 6) + "J";
+		});
+
+	std::string result = orgraph['j'][7]();
+
+	std::cout << orgraph['a'].size() << " with value " << result << std::endl;
 }
