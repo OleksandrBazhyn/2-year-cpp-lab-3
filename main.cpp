29e8d2299e6d23cc72fa6ea929e490cc5f3d6320
OleksandrBazhyn <54002505+OleksandrBazhyn@users.noreply.github.com>
2023-12-02 23:33:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 // scheme 1, nt = 5, a = 6, b = 5, c = 8, d = 5, e = 8, f = 9, g = 4, h = 5, i = 6, j = 8
 // scheme 1:  a->c->g->j, a->d->h->j, b->e->h->j, b->f->i->j
 
-#include "actionsSets.cpp"
-#include "Duga.h"
 #include "Orgraph.h"
 
 #include <iostream>
